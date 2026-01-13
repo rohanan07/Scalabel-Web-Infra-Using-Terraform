ddb2195f02a91bfd8f4b4a8ff8e93cd7bfbb88f7
rohanan07 <rohannalawade927@gmail.com>
2026-01-13 18:39:28 +0530

    Added Cloudwatch

--- asg.tf
+++ asg.tf
@@ -75,37 +75,3 @@ resource "aws_autoscaling_policy" "target_tracking" {
     target_value = 70.0
   }
 }
-
-resource "aws_cloudwatch_metric_alarm" "high_cpu" {
-  alarm_name          = "high-cpu-utilization"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = "120"
-  statistic           = "Average"
-  threshold           = "80"
-  alarm_description   = "This metric monitors ec2 cpu utilization"
-  alarm_actions       = [aws_autoscaling_policy.scale_out.arn]
-
-  dimensions = {
-    AutoScalingGroupName = aws_autoscaling_group.app-asg.name
-  }
-}
-
-resource "aws_cloudwatch_metric_alarm" "low_cpu" {
-  alarm_name          = "low-cpu-utilization"
-  comparison_operator = "LessThanThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = "120"
-  statistic           = "Average"
-  threshold           = "20"
-  alarm_description   = "This metric monitors ec2 cpu utilization"
-  alarm_actions       = [aws_autoscaling_policy.scale_in.arn]
-
-  dimensions = {
-    AutoScalingGroupName = aws_autoscaling_group.app-asg.name
-  }
-}
